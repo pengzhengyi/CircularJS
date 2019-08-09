4a7c177bef11b658e4c39d326761c9c657a72aa3
pengzhengyi <zhengyi_peng@brown.edu>
2019-08-09 15:57:31 +0800

    implement circular layout by specifying specific radius and angle

---
+++ circular.js
@@ -0,0 +1,110 @@
+(function() {
+    /* customizable defaults */
+    const globalRadiusDefault = '0px';
+    const globalAngleDefault = '270deg';
+
+    const circularContainerSelector = '.circular-container';
+    const circularItemSelector = '.circular-item';
+    const circularCenterSelector = '.circular-center';
+
+    const angleRegex = new RegExp('([+-]?\\d*[.]?\\d+)?(π?)(deg|grad|rad|turn)?');
+
+    function applyToPage() {
+        document.querySelectorAll(circularContainerSelector).forEach(placeCircularContainer);
+        document.querySelectorAll(circularCenterSelector).forEach(placeCircularCenter);
+        document.querySelectorAll(circularItemSelector).forEach(placeCircularItem);
+    }
+
+    function placeCircularContainer(element) {
+        element.style.position = 'relative';
+    }
+
+    function placeCircularItem(element) {
+        placeCircularCenter(element);
+        translateElement(element, getRadius(element), getAngle(element));
+    }
+
+    function placeCircularCenter(element) {
+        element.style.position = 'absolute';
+        element.style.top = '50%';
+        element.style.left = '50%';
+        element.style.transform = 'translate(-50%, -50%)';
+    }
+
+    function translateElement(element, radius, angle) {
+        element.style.transform += `rotate(${angle}) translate(${radius}) rotate(-${angle})`;
+    }
+
+    function getRadius(element) {
+        if ('radius' in element.dataset) {
+            return element.dataset.radius || globalRadiusDefault;
+        } else {
+            return getDefaultRadius(element);
+        }
+    }
+
+    function getDefaultRadius(element) {
+        const circularContainer = element.closest(circularContainerSelector);
+        if (circularContainer) {
+            return circularContainer.dataset.radius || globalRadiusDefault;
+        } else {
+            return globalRadiusDefault;
+        }
+    }
+
+    function getAngle(element) {
+        if ('angle' in element.dataset) {
+            return resolveAngle(element.dataset.angle);
+        } else {
+            return globalAngleDefault;
+        }
+    }
+
+    function resolveAngle(angleStr) {
+        if (!angleStr) {
+            return globalAngleDefault;
+        }
+
+        let [, angle, pi, unit] = angleRegex.exec(angleStr);
+        if (!unit) {
+            if (pi) {
+                unit = 'rad';
+            } else if (parseFloat(angle) > 2 * Math.PI) {
+                unit = 'deg';
+            } else {
+                unit = 'rad';
+            }
+        }
+        return adjustAngle(angle, pi, unit);
+    }
+
+    function mod(n, m) {
+        return ((n % m) + m) % m;
+    }
+
+    function parseRadian(radian, pi) {
+        if (pi) {
+            if (!radian) {
+                radian = 1;
+            }
+            return mod(radian + 1.5, 2) * Math.PI;
+        } else {
+            return mod(radian / Math.PI + 1.5, 2) * Math.PI;
+        }
+    }
+
+    function adjustAngle(angle, pi, unit) {
+        switch(unit) {
+        case 'deg':
+            return `${mod(parseFloat(angle) + 270, 360)}${unit}`;
+        case 'grad':
+            return `${mod(parseFloat(angle) + 300, 400)}${unit}`;
+        case 'rad':
+            return `${parseRadian(parseFloat(angle), pi)}${unit}`;
+        case 'turn':
+            return `${mod(parseFloat(angle) + 0.75, 1)}${unit}`;
+        }
+    }
+
+    applyToPage();
+})();
